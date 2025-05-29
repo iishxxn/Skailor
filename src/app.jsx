739d28aac4203b3bd69382d9935856013636301b
iishxxn <ishaankalia.26@gmail.com>
2025-05-29 13:04:27 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Chapters from "./pages/Chapters";
-import ChapterDetail from "./pages/ChapterDetail";
+import Home from "./pages/home";
+import Chapters from "./pages/chapters";
+import ChapterDetail from "./pages/chapterdetail";
 
 export default function App() {
   return (
