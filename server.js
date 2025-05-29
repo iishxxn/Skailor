57be8a85e30cba438666dcbd6d038f90a2a62c20
iishxxn <ishaankalia.26@gmail.com>
2025-05-29 12:18:33 +0530

    Update server.js to serve static files from dist

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-// server.js
 const express = require('express');
 const path = require('path');
 const app = express();
-
-const port = process.env.PORT || 4000;
-
 app.use(express.static(path.join(__dirname, 'dist')));
 
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'), (err) => {
+    if (err) {
+      res.status(500).send('Error loading index.html');
+    }
+  });
 });
 
+
+const port = process.env.PORT || 4000;
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
